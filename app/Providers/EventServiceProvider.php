5eb0f82a1c9f5e995a393366ae19fbe3b7d3d0a8
matheusmoc <matheusandrade009@gmail.com>
2024-11-08 17:12:01 -0300

    feat: added provider to map DriverAvailable in listener

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Events\DriverAvailable;
+use App\Listeners\AssignNewRideToDriver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -18,7 +20,14 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+
+        //mapeamento do evento DriverAvailable e seu listener
+        // Ouvinte que será chamado quando o evento for disparado
+        DriverAvailable::class => [
+            AssignNewRideToDriver::class,  
+        ],
     ];
+    
 
     /**
      * Register any events for your application.
